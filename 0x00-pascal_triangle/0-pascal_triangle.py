3b6f157a89004416c65ee5dd3eaff4c09ae32e31
davidnene <davienesh4@gmail.com>
2022-12-22 14:05:30 +0300

    Fix 0-pascal_triangle.py code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,28 @@
+#!/usr/bin/python3
+"""
+Pascale triangle
+"""
+
+
+def fact(n):
+    if n == 0:
+        return 1
+    prod = 1
+    for i in range(1, n+1):
+        prod = prod * i
+    return prod
+
+
 def pascal_triangle(n):
     """
-    A function that returns a list of lists of integer representing the Pascal’s triangle of n
+    pascal triangle using python
     """
-    triangle = []
     if n <= 0:
-        return triangle
-
-    for z in range(0, n):
-        """
-        A loop to create a list after every iteration in the range
-        """
-        if z == 0:
-            triangle.append([1])
-        else:
-            row = [1]
-            for e in range(z - 1):
-                row.append(triangle[z - 1][e] + triangle[z - 1][e + 1])
-            row.append(1)
-            triangle.append(row)
-
-    return triangle
+        return []
+    List_1 = []
+    for i in range(n):
+        List_2 = []
+        for j in range(i + 1):
+            List_2.append(fact(i)//(fact(j) * fact(i - j)))
+        List_1.append(List_2)
+    return List_1
